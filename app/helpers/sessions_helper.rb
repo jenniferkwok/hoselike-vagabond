16aa752effd3d0eb90f0fcf896e4a08c01014080
jshin8 <joshshin8@gmail.com>
2015-11-16 17:01:53 -0800

    added anonymous user functionality

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,7 +5,11 @@ module SessionsHelper
 	end
 
 	def current_user
-		@current_user ||= User.find(session[:user_id])
+		if session[:user_id] == nil
+			
+		else
+		@current_user = User.find(session[:user_id])
+		end
 	end
 
 	def logged_in?
